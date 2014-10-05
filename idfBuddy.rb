caacef67303270004fbb614558fb7814e17a72cb
oolongtea <obedespina@gmail.com>
2014-10-04 20:07:08 -0400

    prints list of constructions and materials

--- idfBuddy.rb
+++ idfBuddy.rb
@@ -1,57 +1,41 @@
 f = File.open("./Test_IDF.idf", "r")
 
 i = 0
-material_line = []
-construction_line = []
-material_property_list = []
-construction_material_list = []
-space_break = []
+ary = []
 
 f.each_line do |line|
   i += 1
   if line[0..7] == 'Material'
-    material_line << i
-    nextLine = f.gets
-    while nextLine != "\r\n" do
-      if nextLine.index('!')
-        material_property_list << nextLine[nextLine.index('!')+3..nextLine.length-1]
-      end
-      nextLine = f.gets
-    end
+    ary[i] = 'm'
   elsif line[0..12] == 'Construction,'
-    construction_line << i
-    nextLine = f.gets
-    while nextLine != "\r\n" do
-      if nextLine.index('!')
-        construction_material_list << nextLine[nextLine.index('!')+3..nextLine.length-1]
-      end
-      nextLine = f.gets
+    ary[i] = 'c'
+  elsif line == "\r\n"
+    ary[i] = 'b'
+  elsif line[0] == ' '
+    ary[i] = 'p'
+  else
+    ary[i] = 'x'
+  end
+end
+
+
+#print "Constructions: "
+cons =  ary.each_index.select{|j| ary[j] == 'c'}
+#print "Materials: "
+mats = ary.each_index.select{|j| ary[j] == 'm'}
+#p ary.each_index.select{|j| ary[j] == 'p'}
+
+fname = "Test_IDF.idf"
+
+for k in 0..cons.length
+  p File.readlines(fname)[cons[k]]
+  mats.each do |m|
+    if m < cons[k+1]
+      print "\t"
+      p File.readlines(fname)[m] 
     end
   end
-
-  if line == "\r\n"
-    space_break << i
-  end
-
-end
-
-puts "% -------- Constructions -------- %"
-construction_line.each do |j|
-  line = File.readlines("Test_IDF.idf")
-  commaInd = line[j].index(',') - 1 
-  puts line[j][2..commaInd]
-end
-
-puts "% -------- Materials -------- %"
-material_line.each do |j|
-  line = File.readlines("Test_IDF.idf")
-  commaInd = line[j].index(',') - 1 
-  puts line[j][2..commaInd]
-end
-
-puts "% -------- Properties -------- %"
-material_property_list.each do |p|
-  puts p
+  puts ""
 end
 
 f.close
