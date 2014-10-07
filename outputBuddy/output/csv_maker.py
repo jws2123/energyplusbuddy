82e1700b3164b8461ac12517afedafedb99cbf27
jws2123 <jws2123@gmail.com>
2014-10-06 20:08:02 -0400

    because josh took a big poo in the outputBuddy/output folder

--- outputBuddy/output/csv_maker.py
+++ outputBuddy/output/csv_maker.py
@@ -1,6 +1,6 @@
 import csv
 
-for i in range(0,7985):
+for i in range(1,7986):
 	with open (str(i) + ".csv", "wb") as file:
 		writer = csv.writer(file, delimiter=',')
 		writer.writerow(["energy_output1", "energy_output2", "energy_output3", "energy_output4"])
