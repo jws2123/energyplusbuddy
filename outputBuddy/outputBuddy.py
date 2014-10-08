fa62c0ee7ede57f0482d6e61aead666fc725c831
jws2123 <jws2123@gmail.com>
2014-10-08 09:06:22 -0400

    outputBuddy rewritten with pandas

--- outputBuddy/outputBuddy.py
+++ outputBuddy/outputBuddy.py
@@ -1,53 +1,29 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
 
 
 output = pd.read_csv("../combinations/output.csv")
 
-location = "/output"
+outputFileLocation = "output"
 
 count = 0
-for (dirname, dirs, files) in os.walk(location):
+for (dirname, dirs, files) in os.walk(outputFileLocation):
 	for filename in files:
 		if filename.endswith(".csv"):
 			count = count + 1
 
-f = open("output/1.csv")
-columns = f.readline
-f.close()
+df = pd.read_csv(outputFileLocation + "/1.csv")
 
-index=range(0,count)
-
-#creating an empty dataframe
-results_compiled = DataFrame(index=index, columns=columns)
+index=range(2,count+1)
 
 for i in index:
-	df = pd.read_csv("location/" + str(i) + ".csv")
-	transpose = df.T
+	tempDf = pd.read_csv(outputFileLocation + "/" + str(i) + ".csv")
+	df = df.append(tempDf.iloc[0])
+
+print df
 	
-	
-
-#f = open(str("1.csv", "r")
-#output = open("output.csv", "a")
-#f.readline
-
-#output.append(
-
-#probably going to need to re-write this with pandas... not sure if you can append one line at a time the way we need to.
-
-#for files in range(0,count):
-	
-#	f = open(str(files+1) + ".csv", "r")
-#	output = open("output.csv", "a")
-	
-	
-
-#	for line in f:
-#		if line == (files+1) :
-#			output.append("," + line) 
-
-
-
 
 
