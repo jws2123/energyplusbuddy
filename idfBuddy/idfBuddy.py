e5a828f31551c0bfd15b2c4208884f46db7e15d2
jws2123 <jws2123@gmail.com>
2015-01-17 18:09:35 -0500

    template adjusted

--- idfBuddy/idfBuddy.py
+++ idfBuddy/idfBuddy.py
@@ -6,3 +6,14 @@ file = sys.argv[1]
 material = sys.argv[2]
 variable = sys.argv[3]
 var_value = sys.argv[4]
+
+with open(file) as f:
+	for line in f:
+		line = line.strip()
+		line = line.upper()
+		material = material.upper()
+		if line == material:
+			print line
+		##else: 
+		##	print 0
+
