4f80052ac850cc2855b198e000916525698fbf69
jws2123 <jws2123@gmail.com>
2015-01-17 21:02:59 -0500

    YES! IDF parser seems to work.

--- idfBuddy/idfBuddy.py
+++ idfBuddy/idfBuddy.py
@@ -1,19 +1,47 @@
 import sys
-
-#print "\n".join(sys.argv[1:])
+import os
+#import time
 
 file = sys.argv[1]
 material = sys.argv[2]
 variable = sys.argv[3]
 var_value = sys.argv[4]
+found_material_definition = False
+found_material = False
+found_variable = False
+
+split = file.split(".")
+split[-2] = split[-2]+"_temp"
+newfile = '.'.join(split)
+
+print newfile
+				
+new_f = open(newfile, 'w+')
 
 with open(file) as f:
 	for line in f:
-		line = line.strip()
-		line = line.upper()
-		material = material.upper()
-		if line == material:
-			print line
-		##else: 
-		##	print 0
+		temp_line = line.strip()
+		temp_line = temp_line.upper()
+		if found_material:
+			variable = variable.upper()
+			if variable in temp_line:
+				split = line.split(",")
+				split[0] = "  " + var_value
+				line = ",".join(split)
+			if ";" in temp_line:
+				found_material = False		
+		if found_material_definition:
+			material = material.strip()
+			material = material.upper()
+			if material in temp_line:
+				found_material = True
+			found_material_definition = False
+		else:
+			if temp_line == "MATERIAL," or temp_line == "WINDOWMATERIAL:GLAZING,":
+				found_material_definition = True
+		new_f.write(line)
+
+new_f.close()
+
+os.system("mv " + newfile + " " + file)
 
